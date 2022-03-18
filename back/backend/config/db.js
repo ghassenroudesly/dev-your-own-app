4395f75c0e109eb602da1fea1115196de933f9de
ghassenroudesly <roudeslyroudesly@gmail.com>
2022-03-18 12:24:57 +0100

    make it bett

--- back/backend/config/db.js
+++ back/backend/config/db.js
@@ -4,7 +4,7 @@ import colors from 'colors'
 // a mongoose stuf (mongoose.connect ....) return always a promise
 const connectDB = async () => {
     try{
-        const conn = await mongose.connect(process.env.MONGO_URI,{
+        const conn = await mongose.connect("mongodb+srv://ghassen:Ghassen12ghassen@cluster0.u636b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true
